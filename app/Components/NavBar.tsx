db07deec21a41cfe1f337de9c26bae616f304f37
Ronyl-V <kenfackronyl1@gmail.com>
2025-06-22 00:50:48 +0100

    NavBar.tsx

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <div className="bg-white lg:bg-white/80 lg:backdrop-blur-sm border-b border-gray-200 sticky top-0 z-50">
+    <div className="bg-white lg:bg-white/80 border-b border-gray-200 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <Link href="/" className="flex items-center space-x-2 cursor-pointer">
